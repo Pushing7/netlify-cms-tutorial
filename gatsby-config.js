d2028ccb84c56eddfa1cd67c93643f0d3c9297cb
Ben Caldwell <bcaldwell@pushing7.com>
2023-02-16 12:30:15 -0600

    first crack and custom preview css

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,16 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-   plugins: [`gatsby-plugin-netlify-cms`],
+   plugins: [
+      {
+         resolve: `gatsby-plugin-netlify-cms`,
+         options: {
+           /**
+            * One convention is to place your Netlify CMS customization code in a
+            * `src/cms` directory.
+            */
+           modulePath: `${__dirname}/src/cms/cms.js`,
+         },
+       },
+   ],
 }
